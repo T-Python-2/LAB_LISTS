fafe894fd1663b7939cae58d8e3cbf2f75508282
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2022-04-21 13:42:31 +0300

    lists lab

--- lists.py
+++ lists.py
@@ -9,7 +9,7 @@ max_list = [4,1,66,5]
 print("Largest number in list = " , max(max_list))
 
 
-range_list = [x for x in range(1200,2000,125) if x%2 != 0]
+range_list = [x for x in range(1200,2000,125)]
 
 print(range_list)
 
