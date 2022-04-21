ba25e67128a3544d9e898427e8b4b0a156f54c15
alimatrood <alisaeedalmatrood@gmail.com>
2022-04-21 13:36:06 +0300

    solved all questions

---
+++ Lists_Lab.py
@@ -0,0 +1,28 @@
+#LISTS LAB
+
+# A function to sum all items in a list
+from typing import List
+
+#A function to sum all items in a list
+def sum_items(list:List):
+    sum = 0
+    for element in list:
+        sum+=element
+    print("The sum is {}".format(sum))
+
+sum_items([1,2,3,4,5,6])
+
+#A function to get the largest number from a list
+def largest_number(list:List):
+    list.sort()
+    print("The largest number is {}".format(list[-1]))
+
+largest_number([5,0,9,2,3,7,6])
+
+#odd numbers list in the range 1200-2000 with steps of 125 using list comprehension
+odd_numbers_list = [number for number in range(1200,2000,125)]
+print(odd_numbers_list)
+
+#using list slicing to get the list from the first element to the 5th element
+sliced_list = odd_numbers_list[:5]
+print(sliced_list)
