0247ef640c062657466e2c9181570f4c220d7684
Eman Samkri <Eman.Jameel.jed.k2@tuwaiq.edu.sa>
2022-04-21 14:39:27 +0300

    updated using list comprehension

--- main.py
+++ main.py
@@ -12,18 +12,21 @@ print (sum)
 print(max(my_list))
 
 #Q3
+'''
 odd_list = list()
 for element in range(1200,2000,125):
     if element % 2 == 0:
         continue
     else:
         odd_list.append(element)
-
 print(odd_list)
+'''
+#using 
 
-
-#Q5
+odd_list2 =[ n for n in range(1200,2000,125) if n % 2 != 0 ]
+print(odd_list2)
+#Q4
 scal_list = list()
 new_list = [1,2,3,4,5,6,7,8]
-scal_list = new_list[4:]
+scal_list = new_list[:5]
 print(scal_list)
