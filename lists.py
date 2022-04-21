312436e433a6980ce7369e3471de4563bb829720
AbdulghaniAlhazmi <Abdulghani.Hassan.jed.k2@tuwaiq.edu.sa>
2022-04-21 13:41:04 +0300

    lists lab

---
+++ lists.py
@@ -0,0 +1,23 @@
+
+numbers_list = [1,2,3,4,5]
+
+print(" Sum of the list = " , sum(numbers_list))
+
+
+max_list = [4,1,66,5]
+
+print("Largest number in list = " , max(max_list))
+
+
+range_list = [x for x in range(1200,2000,125) if x%2 != 0]
+
+print(range_list)
+
+
+print (range_list[5:])
+
+
+
+
+
+
